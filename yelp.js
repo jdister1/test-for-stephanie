0ef9e639c30ad4f3c1059abd7cfb09beff17eac4
Joe <joe@reformlabs.io>
2021-07-01 16:24:13 -0400

    trying s Code

--- yelp.js
+++ yelp.js
@@ -1,13 +1,17 @@
+let apiKey = "N6jC9hEJzTF9RnctCg_sNYHsnJeGGqXljv7PadDwa9cnNkH1l-dyPYCqUZ3j6JFyEBP9kfiiGvNbjdGloQd-0trLXbfSMkA69e1gvRnJM3q5ps_T1Z7-yZxkWg7dYHYx"
 
+//format the yelp api url
+var apiUrl = "https://api.yelp.com/v3/businesses/search?location=Cleveland&radius=40000"
 
-let apiKey =
-  "N6jC9hEJzTF9RnctCg_sNYHsnJeGGqXljv7PadDwa9cnNkH1l-dyPYCqUZ3j6JFyEBP9kfiiGvNbjdGloQd-0trLXbfSMkA69e1gvRnJM3q5ps_T1Z7-yZxkWg7dYHYx";
+//make a request to the url
 
-fetch("https://api.yelp.com/v3/businesses/search?location=Cleveland&radius=40000", {
-  headers: new Headers({
-    Authorization: "Bearer " + apiKey,
-  }),
-  method: "GET",
+fetch(apiUrl, {
+    headers: new Headers({
+        Authorization: "Bearer " + apiKey,
+        "Access-Control-Allow-Origin" : "*"
+    }),
+    method: "GET",
+    mode:"cors"
 })
 .then((response) => response.json())
 .then((data) => console.log(data));
